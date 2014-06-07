a3dd43d5a53d5dd53da48886b85d92789dee774f
daljeet <daljeet_arora@yahoo.com>
2014-06-07 18:00:10 +0530

    first commit for plot2

---
+++ plot2.R
@@ -0,0 +1,18 @@
+#C:\\amdocs\\Big Data\\Coursera\\Exploratory Data analysis\\Week 1\\Project\\ExData_Plotting1
+
+
+datafile = "household_power_consumption.txt"
+datatable <- read.table(datafile, sep = ";", header = TRUE)
+
+
+relvnt_datatable <- datatable[datatable$Date == "1/2/2007" | datatable$Date == "2/2/2007" ,]
+
+#For Plot2
+dt <- as.Date(relvnt_datatable$Date, "%d/%m/%Y")
+time <- as.character(relvnt_datatable$Time)
+comb <- paste(dt, time)
+
+date_time <- strptime(comb, format = "%Y-%m-%d %H:%M:%S")
+
+plot(date_time, as.numeric(as.character(relvnt_datatable$Global_active_power)), type = "l")
+
