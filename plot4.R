e85b237497ec9ca3099e42cde447f7ee388e0961
daljeet <daljeet_arora@yahoo.com>
2014-06-07 22:58:29 +0530

    corrected y-lable for one of them

--- plot4.R
+++ plot4.R
@@ -5,16 +5,17 @@ datatable <- read.table(datafile, sep = ";", header = TRUE)
 
 relvnt_datatable <- datatable[datatable$Date == "1/2/2007" | datatable$Date == "2/2/2007" ,]
 
-#For Plot2
+png(filename = "plot4.png")
 par(mfcol = c(2,2))
 
+#For Plot2
 dt <- as.Date(relvnt_datatable$Date, "%d/%m/%Y")
 time <- as.character(relvnt_datatable$Time)
 comb <- paste(dt, time)
 
 date_time <- strptime(comb, format = "%Y-%m-%d %H:%M:%S")
 
-ylab = "Global Active Power (kilowatts)"
+ylab = "Global Active Power"
 xlab = ""
 
 plot(date_time, as.numeric(as.character(relvnt_datatable$Global_active_power)), type = "l", xlab = xlab, ylab = ylab)
@@ -32,10 +33,11 @@ lines(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_3)), type
 
 #For Plot3 - Annotate
 legend <- c(colnames(relvnt_datatable)[7], colnames(relvnt_datatable)[8], colnames(relvnt_datatable)[9])
-legend("topright", lty = 1, col = c("black", "red","blue"), legend = legend)
+legend("topright", bty = "n", lty = 1, col = c("black", "red","blue"), legend = legend)
 
 
 #For new plots
-plot(date_time, as.numeric(as.character(relvnt_datatable$Voltage)), type = "l", xlab = "datetime", ylab = "VOltage")
+plot(date_time, as.numeric(as.character(relvnt_datatable$Voltage)), type = "l", xlab = "datetime", ylab = "Voltage")
 plot(date_time, as.numeric(as.character(relvnt_datatable$Global_reactive_power)), type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
+dev.off()
