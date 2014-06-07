a34cf43414d78596a969b95b7b561f24373950ad
daljeet <daljeet_arora@yahoo.com>
2014-06-07 18:11:59 +0530

    udated for labels

--- plot2.R
+++ plot2.R
@@ -4,7 +4,7 @@
 datafile = "household_power_consumption.txt"
 datatable <- read.table(datafile, sep = ";", header = TRUE)
 
-
+#hard coding values
 relvnt_datatable <- datatable[datatable$Date == "1/2/2007" | datatable$Date == "2/2/2007" ,]
 
 #For Plot2
@@ -14,5 +14,8 @@ comb <- paste(dt, time)
 
 date_time <- strptime(comb, format = "%Y-%m-%d %H:%M:%S")
 
-plot(date_time, as.numeric(as.character(relvnt_datatable$Global_active_power)), type = "l")
+ylab = "Global Active Power (kilowatts)"
+xlab = ""
+
+plot(date_time, as.numeric(as.character(relvnt_datatable$Global_active_power)), type = "l", xlab = xlab, ylab = ylab)
 
