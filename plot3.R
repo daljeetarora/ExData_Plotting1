046b2d0861d7afc3faf1d7453335395e62d076ff
daljeet <daljeet_arora@yahoo.com>
2014-06-07 19:49:22 +0530

    first commit for plot3

---
+++ plot3.R
@@ -0,0 +1,28 @@
+#C:\\amdocs\\Big Data\\Coursera\\Exploratory Data analysis\\Week 1\\Project\\ExData_Plotting1
+
+datafile = "household_power_consumption.txt"
+datatable <- read.table(datafile, sep = ";", header = TRUE)
+
+
+relvnt_datatable <- datatable[datatable$Date == "1/2/2007" | datatable$Date == "2/2/2007" ,]
+
+
+dt <- as.Date(relvnt_datatable$Date, "%d/%m/%Y")
+time <- as.character(relvnt_datatable$Time)
+comb <- paste(dt, time)
+
+date_time <- strptime(comb, format = "%Y-%m-%d %H:%M:%S")
+
+#png()
+
+plot(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_1)), type = "n", xlab = "test", ylab = "hkhk")
+
+lines(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_1)), type = "l")
+lines(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_2)), type = "l", col = "red")
+lines(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_3)), type = "l", col = " blue")
+
+#For Plot3 - Annotate
+legend <- c(colnames(relvnt_datatable)[7], colnames(relvnt_datatable)[8], colnames(relvnt_datatable)[9])
+legend("topright", pch = 5, col = c("black", "red","blue"), legend = legend)
+
+#dev.off()
