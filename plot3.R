be3b5a6cc814f7df20b3511cda844dd41ca2de8f
daljeet <daljeet_arora@yahoo.com>
2014-06-07 19:57:17 +0530

    labels

--- plot3.R
+++ plot3.R
@@ -3,9 +3,7 @@
 datafile = "household_power_consumption.txt"
 datatable <- read.table(datafile, sep = ";", header = TRUE)
 
-
 relvnt_datatable <- datatable[datatable$Date == "1/2/2007" | datatable$Date == "2/2/2007" ,]
-
 
 dt <- as.Date(relvnt_datatable$Date, "%d/%m/%Y")
 time <- as.character(relvnt_datatable$Time)
@@ -15,7 +13,10 @@ date_time <- strptime(comb, format = "%Y-%m-%d %H:%M:%S")
 
 #png()
 
-plot(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_1)), type = "n", xlab = "test", ylab = "hkhk")
+ylab = "Energy sub metering"
+xlab = ""
+
+plot(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_1)), type = "n", xlab = xlab, ylab = ylab)
 
 lines(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_1)), type = "l")
 lines(date_time, as.numeric(as.character(relvnt_datatable$Sub_metering_2)), type = "l", col = "red")
