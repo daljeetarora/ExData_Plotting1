07e67e7d614f953596e51bb1b0d35f00dbbc246f
daljeet <daljeet_arora@yahoo.com>
2014-06-07 17:00:33 +0530

    added labels

--- plot1.R
+++ plot1.R
@@ -1,10 +1,12 @@
 #C:\\amdocs\\Big Data\\Coursera\\Exploratory Data analysis\\Week 1\\Project\\ExData_Plotting1
 
-colClasses <- c("character")
 datafile = "household_power_consumption.txt"
 datatable <- read.table(datafile, sep = ";", header = TRUE)
 
-
+#hard coding values
 relvnt_datatable <- datatable[datatable$Date == "1/2/2007" | datatable$Date == "2/2/2007" ,]
 
-hist(as.numeric(as.character(relvnt_datatable$Global_active_power)), col = "red")
+xlab = "Global Active Power (kilowatts)"
+main = "Global Active Power"
+
+hist(as.numeric(as.character(relvnt_datatable$Global_active_power)), col = "red", xlab = xlab)
